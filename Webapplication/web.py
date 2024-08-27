dc0f5b5d38d4b75a70ad2cddb0cfd7e059405a29
prince3453 <princeghoghari9@gmail.com>
2024-08-27 14:05:54 -0400

    Adding complete items implement in Todo Item

--- Webapplication/web.py
+++ Webapplication/web.py
@@ -11,7 +11,12 @@ st.title("My Todo App")
 st.subheader("My Todo App")
 st.write("The app is for everyday work")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.file_write(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add new Todo...", on_change=add_todo, key='new_todo')
